02328c8e0d66df8be93e7e7e2be8b8a96555472c
mightymax <mark@lindeman.nu>
2024-08-18 14:39:11 +0200

    Useless file deleted

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-TARGET=/home/www/vinyl.lindeman.nu
-rsync -Cavz --filter=':- .gitignore' . home.lindeman.nu:$TARGET
-ssh home.lindeman.nu sudo docker-compose --project-directory $TARGET down
-ssh home.lindeman.nu sudo docker-compose --project-directory $TARGET up --build -d
